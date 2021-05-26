51fb00918c9bdf369cb20517bf967755386867c5
Frank Valdez <fvaldez421@gmail.com>
2021-05-25 21:41:31 -0700

    added starter job list workflow

--- pages/dashboard/script.js
+++ pages/dashboard/script.js
@@ -1,1 +1,34 @@
 console.log('hello world')
+
+const body = document.querySelector('body')
+
+
+function addJobsToBody(jobs) {
+  if (jobs && jobs.length > 0) {
+    const list = document.createElement('ul')
+  
+    for (let i = 0; i < jobs.length; i++) {
+      const job = jobs[i]
+      const li = document.createElement('li')
+      li.innerText = job.name
+      list.appendChild(li)
+    }
+  
+    body.appendChild(list)
+  }
+}
+
+
+fetch('http://127.0.0.1:5000/jobs')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    console.log(body)
+    if (data && data.jobs) {
+      addJobsToBody(data.jobs)
+    }
+  })
+
+
+
+
